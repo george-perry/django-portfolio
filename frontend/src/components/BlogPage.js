b4a2125ccaa94545667addf668683fe121837fb6
George Perry <gep1617@gmail.com>
2025-02-12 12:39:36 -0500

    Fix blog page ids

--- frontend/src/components/BlogPage.js
+++ frontend/src/components/BlogPage.js
@@ -42,7 +42,7 @@ export default class BlogPage extends Component {
         <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
           <div className="grid gap-8 lg:grid-cols-3 sm:max-w-sm sm:mx-auto lg:max-w-full">
             {this.state.data.map(post => {
-              let url = `/blog/${post.project.id}`
+              let url = `/blog/${post.id}`
               {
                 if (post.title !== "About Me") {
                   return;
